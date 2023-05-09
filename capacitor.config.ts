a39ac24dfd2ee4954d470849a7ee47ce78cdbb2b
mufniarz <marcinmu330@gmail.com>
2023-05-09 16:31:38 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ascendcoopplatform',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;

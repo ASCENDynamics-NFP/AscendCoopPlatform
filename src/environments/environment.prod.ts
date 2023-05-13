f462bc91bb42a531c3fbc8be519e53427b3970b3
mufniarz <marcinmu330@gmail.com>
2023-05-14 00:26:27 +0800

    adding databaseURL, though probably shouldn't

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   firebaseConfig: {
     apiKey: "your-api-key",
     authDomain: "your-project-id.firebaseapp.com",
+    databaseURL: "https://your-project-id.firebaseio.com",
     projectId: "your-project-id",
     storageBucket: "your-project-id.appspot.com",
     messagingSenderId: "your-messaging-sender-id",

44c715ea27522cb3060ae2136893494364245bd1
mufniarz <marcinmu330@gmail.com>
2023-05-18 09:41:51 +0800

    styled html, added signup ui and functions

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,23 @@
 import {User} from "firebase/auth"; // import User type
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, Observable} from "rxjs";
 import {Injectable} from "@angular/core";
 import {
   getAuth,
   createUserWithEmailAndPassword,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signInWithEmailLink,
+  sendPasswordResetEmail,
   signOut,
 } from "firebase/auth";
+// import { isPlatform } from "@ionic/angular";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
   auth = getAuth();
-  private userSubject = new BehaviorSubject<User | null>(null); // User | null type
+  private userSubject = new BehaviorSubject<User | boolean>(false); // User | false type
   user$ = this.userSubject.asObservable();
   constructor() {
     onAuthStateChanged(this.auth, (user) => {
@@ -23,7 +26,7 @@ export class AuthService {
         this.userSubject.next(user);
       } else {
         // User is signed out.
-        this.userSubject.next(null);
+        this.userSubject.next(false);
       }
     });
   }
@@ -57,6 +60,30 @@ export class AuthService {
     }
   }
 
+  onSendPasswordResetEmail(email: string): Promise<void> {
+    try {
+      const result = sendPasswordResetEmail(this.auth, email);
+      return result;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  // async onSignInWithEmailLink(email: string) {
+  //   const redirectTo = isPlatform('capacitor') ? 'firebase://login' : `${window.location.origin}/groups`;
+  //   console.log('set redirect: ', redirectTo);
+  //   try {
+  //     const result = await signInWithEmailLink(
+  //     this.auth,
+  //     email,);
+  //     return result;
+  //   } catch (error) {
+  //     console.log(error);
+  //     throw error;
+  //   }
+  // }
+
   // Sign out
   async signOut() {
     try {
@@ -65,5 +92,9 @@ export class AuthService {
       console.log(error);
       throw error;
     }
+  }
+
+  getCurrentUser(): Observable<User | boolean> {
+    return this.user$;
   }
 }

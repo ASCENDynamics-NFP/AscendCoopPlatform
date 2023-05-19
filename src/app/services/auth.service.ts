035183fe6e55a51e4a1ecff34317a85900839dd1
mufniarz <marcinmu330@gmail.com>
2023-05-19 09:54:47 +0800

    added email link login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,9 @@
-import {User} from "firebase/auth"; // import User type
-import {BehaviorSubject, Observable} from "rxjs";
+import {
+  User,
+  isSignInWithEmailLink,
+  sendSignInLinkToEmail,
+} from "firebase/auth"; // import User type
+import {BehaviorSubject} from "rxjs";
 import {Injectable} from "@angular/core";
 import {
   getAuth,
@@ -10,7 +14,6 @@ import {
   sendPasswordResetEmail,
   signOut,
 } from "firebase/auth";
-// import { isPlatform } from "@ionic/angular";
 
 @Injectable({
   providedIn: "root",
@@ -20,6 +23,22 @@ export class AuthService {
   private userSubject = new BehaviorSubject<User | boolean>(false); // User | false type
   // any time the user logs in or out (i.e., when currentUserSubject is updated), all subscribers to currentUser$ will receive the updated user state.
   user$ = this.userSubject.asObservable();
+  actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be in the authorized domains list in the Firebase Console.
+    url: `${window.location.origin}/user-login`,
+    // This must be true.
+    handleCodeInApp: true,
+    // iOS: {
+    //   bundleId: 'com.example.ios'
+    // },
+    // android: {
+    //   packageName: 'com.example.android',
+    //   installApp: true,
+    //   minimumVersion: '12'
+    // },
+    // dynamicLinkDomain: 'example.page.link'
+  };
 
   constructor() {
     onAuthStateChanged(this.auth, (user) => {
@@ -73,19 +92,54 @@ export class AuthService {
     }
   }
 
-  // async onSignInWithEmailLink(email: string) {
-  //   const redirectTo = isPlatform('capacitor') ? 'firebase://login' : `${window.location.origin}/groups`;
-  //   console.log('set redirect: ', redirectTo);
-  //   try {
-  //     const result = await signInWithEmailLink(
-  //     this.auth,
-  //     email,);
-  //     return result;
-  //   } catch (error) {
-  //     console.log(error);
-  //     throw error;
-  //   }
-  // }
+  async onSendSignInLinkToEmail(email: string) {
+    try {
+      const result = await sendSignInLinkToEmail(
+        this.auth,
+        email,
+        this.actionCodeSettings,
+      );
+      return result;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  onSignInWithEmailLink() {
+    // Confirm the link is a sign-in with email link.
+    if (isSignInWithEmailLink(this.auth, window.location.href)) {
+      // Additional state parameters can also be passed via URL.
+      // This can be used to continue the user's intended action before triggering
+      // the sign-in operation.
+      // Get the email if available. This should be available if the user completes
+      // the flow on the same device where they started it.
+      let email = window.localStorage.getItem("emailForSignIn");
+      if (!email) {
+        // User opened the link on a different device. To prevent session fixation
+        // attacks, ask the user to provide the associated email again. For example:
+        email = window.prompt("Please provide your email for confirmation");
+      }
+
+      if (email != null) {
+        // The client SDK will parse the code from the link for you.
+        signInWithEmailLink(this.auth, email, window.location.href)
+          .then((result) => {
+            // Clear email from storage.
+            window.localStorage.removeItem("emailForSignIn");
+            // You can access the new user via result.user
+            // Additional user info profile not available via:
+            // result.additionalUserInfo.profile == null
+            // You can check if the user is new or existing:
+            // result.additionalUserInfo.isNewUser
+          })
+          .catch((error) => {
+            // Some error occurred, you can inspect the code: error.code
+            // Common errors could be invalid email and invalid or expired OTPs.
+          });
+      }
+    }
+  }
 
   // Sign out
   async signOut() {

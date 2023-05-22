0ed674683fdd50576fcbdda0c310079998d5ff3c
mufniarz <marcinmu330@gmail.com>
2023-05-22 11:57:40 +0800

    ran formatting

--- src/app/guards/secure-inner-pages.guard.ts
+++ src/app/guards/secure-inner-pages.guard.ts
@@ -6,15 +6,19 @@ import {
 } from "@angular/router";
 import {Observable} from "rxjs";
 import {AuthService} from "../services/auth.service";
-import { MenuController } from "@ionic/angular";
+import {MenuController} from "@ionic/angular";
 
 @Injectable({
   providedIn: "root",
 })
 export class SecureInnerPagesGuard {
-  constructor(public authService: AuthService, public router: Router,
-    public menuCtrl: MenuController,) {
-    this.menuCtrl.enable(false);}
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+    public menuCtrl: MenuController,
+  ) {
+    this.menuCtrl.enable(false);
+  }
 
   // Used to restrict pages to users when they are logged in
   canActivate(

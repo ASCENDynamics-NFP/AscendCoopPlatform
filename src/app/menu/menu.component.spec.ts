f2c88628194390155442f4095d26de0df6927238
mufniarz <marcinmu330@gmail.com>
2023-05-28 05:07:13 -0500

    fixed unit tests

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,16 +1,27 @@
 import {ComponentFixture, TestBed} from "@angular/core/testing";
 import {MenuComponent} from "./menu.component";
 import {RouterTestingModule} from "@angular/router/testing";
+import { AuthService } from "../services/auth.service";
+import { of } from "rxjs";
 
 describe("MenuComponent", () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
+  let service: AuthService;
+  let authSpy: any;
 
   beforeEach(() => {
+    authSpy = jasmine.createSpyObj("auth", ["onSignOut"]);
+    // Mock user$ as an Observable that emits null
+    authSpy.user$ = of(null);
+    authSpy.onSignOut.and.returnValue(Promise.resolve());
+    
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
+      providers: [{provide: AuthService, useValue: authSpy}],
     }).compileComponents();
 
+    service = TestBed.inject(AuthService);
     fixture = TestBed.createComponent(MenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();

8958b6bd80f840ce57650ed5efc3393a2a937cf9
mufniarz <marcinmu330@gmail.com>
2023-07-25 14:36:35 +0200

    fixed unit test error and formatted files

--- src/app/modules/user/pages/edit-user-profile/edit-user-profile.page.spec.ts
+++ src/app/modules/user/pages/edit-user-profile/edit-user-profile.page.spec.ts
@@ -1,11 +1,27 @@
 import {ComponentFixture, TestBed} from "@angular/core/testing";
 import {EditUserProfilePage} from "./edit-user-profile.page";
+import {ActivatedRoute} from "@angular/router";
 
 describe("EditUserProfilePage", () => {
   let component: EditUserProfilePage;
   let fixture: ComponentFixture<EditUserProfilePage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: () => "123", // provide your mock value here
+              },
+            },
+          },
+        },
+      ],
+    }).compileComponents();
     fixture = TestBed.createComponent(EditUserProfilePage);
     component = fixture.componentInstance;
     fixture.detectChanges();

ae92da26929bdbf8864df1971d74cd42693eef4f
mufniarz <marcinmu330@gmail.com>
2023-08-01 10:43:36 +0200

    fixed failed unit test

--- src/app/modules/group/components/create-group-modal/create-group-modal.component.spec.ts
+++ src/app/modules/group/components/create-group-modal/create-group-modal.component.spec.ts
@@ -28,7 +28,6 @@ describe("CreateGroupModalComponent", () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [CreateGroupModalComponent],
       imports: [IonicModule.forRoot()],
     }).compileComponents();
 

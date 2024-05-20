5f61d58474d22c72bb658cfd2b7230a9b35c0953
mufniarz <marcinmu330@gmail.com>
2024-05-20 19:54:29 +0800

    updated unit tests for settings page

--- src/app/modules/account/pages/settings/settings.page.spec.ts
+++ src/app/modules/account/pages/settings/settings.page.spec.ts
@@ -17,75 +17,98 @@
 * You should have received a copy of the GNU Affero General Public License
 * along with Nonprofit Social Networking Platform.  If not, see <https://www.gnu.org/licenses/>.
 ***********************************************************************************************/
-import {ComponentFixture, TestBed, waitForAsync} from "@angular/core/testing";
-import {SettingsPage} from "./settings.page";
-import {
-  TranslateService,
-  TranslateModule,
-  TranslateLoader,
-} from "@ngx-translate/core";
-import {HttpClientTestingModule} from "@angular/common/http/testing";
-import {HttpClient} from "@angular/common/http";
-import {createTranslateLoader} from "../../../../app.component.spec";
-import {ActivatedRoute} from "@angular/router";
+import {TestBed, ComponentFixture, waitForAsync} from "@angular/core/testing";
 import {IonicModule} from "@ionic/angular";
-import {AuthStoreService} from "../../../../core/services/auth-store.service";
+import {ReactiveFormsModule, FormBuilder} from "@angular/forms";
+import {TranslateService} from "@ngx-translate/core";
 import {StoreService} from "../../../../core/services/store.service";
+import {SettingsComponent} from "./components/settings/settings.component";
+import {User} from "firebase/auth";
+import {Account} from "../../../../models/account.model";
 
-const mockStoreService = {
-  loadInitialData: jasmine.createSpy("loadInitialData"),
-  getCollection: jasmine.createSpy("getCollection"),
-  // ... other methods and properties you want to mock
-};
-
-describe("UserSettingsPage", () => {
-  let component: SettingsPage;
-  let fixture: ComponentFixture<SettingsPage>;
-
-  // Mock the AuthStoreService
-  const mockAuthStoreService = {
-    getCurrentUser: jasmine.createSpy("getCurrentUser").and.returnValue({
-      /* mock user data */
-    }),
-  };
+describe("SettingsComponent", () => {
+  let component: SettingsComponent;
+  let fixture: ComponentFixture<SettingsComponent>;
+  let storeService: jasmine.SpyObj<StoreService>;
+  let translateService: jasmine.SpyObj<TranslateService>;
 
   beforeEach(waitForAsync(() => {
+    const storeServiceSpy = jasmine.createSpyObj("StoreService", ["updateDoc"]);
+    const translateServiceSpy = jasmine.createSpyObj("TranslateService", [
+      "use",
+    ]);
+
     TestBed.configureTestingModule({
+      imports: [IonicModule.forRoot(), ReactiveFormsModule],
       declarations: [],
-      imports: [
-        HttpClientTestingModule,
-        TranslateModule.forRoot({
-          loader: {
-            provide: TranslateLoader,
-            useFactory: createTranslateLoader,
-            deps: [HttpClient],
-          },
-        }),
-        IonicModule, // You might need this if your component uses Ionic components
-      ],
       providers: [
-        TranslateService,
-        {provide: AuthStoreService, useValue: mockAuthStoreService}, // Use the mock service
-        {provide: StoreService, useValue: mockStoreService},
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            snapshot: {
-              paramMap: {
-                get: () => "123",
-              },
-            },
-          },
-        },
+        FormBuilder,
+        {provide: StoreService, useValue: storeServiceSpy},
+        {provide: TranslateService, useValue: translateServiceSpy},
       ],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(SettingsPage);
+    fixture = TestBed.createComponent(SettingsComponent);
     component = fixture.componentInstance;
+    storeService = TestBed.inject(StoreService) as jasmine.SpyObj<StoreService>;
+    translateService = TestBed.inject(
+      TranslateService,
+    ) as jasmine.SpyObj<TranslateService>;
+
     fixture.detectChanges();
   }));
 
-  it("should create", () => {
+  it("should create the settings component", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should initialize form with default values", () => {
+    expect(component.settingsForm.value).toEqual({
+      privacy: "public",
+      language: "en",
+    });
+  });
+
+  it("should update the form when account input changes", () => {
+    const account: Partial<Account> = {
+      privacy: "private",
+      accessibility: {preferredLanguage: "fr"},
+    };
+    component.account = account;
+    component.ngOnChanges();
+    expect(component.settingsForm.value).toEqual({
+      privacy: "private",
+      language: "fr",
+    });
+  });
+
+  it("should emit language change event and call translate service on language change", () => {
+    spyOn(component.languageChange, "emit");
+    component.settingsForm.patchValue({language: "fr"});
+    component.onLanguageChange();
+    expect(translateService.use).toHaveBeenCalledWith("fr");
+    expect(component.languageChange.emit).toHaveBeenCalledWith("fr");
+  });
+
+  it("should update settings in the store when updateSetting is called", () => {
+    const authUser: User = {uid: "12345"} as User;
+    component.authUser = authUser;
+    component.settingsForm.patchValue({privacy: "private", language: "fr"});
+    component.updateSetting();
+    expect(storeService.updateDoc).toHaveBeenCalledWith("accounts", {
+      id: "12345",
+      privacy: "private",
+      accessibility: {preferredLanguage: "fr"},
+    });
+  });
+
+  it("should toggle dark theme", () => {
+    const event = {detail: {checked: true}} as CustomEvent;
+    component.toggleDarkTheme(event);
+    expect(document.body.classList.contains("dark")).toBe(true);
+
+    event.detail.checked = false;
+    component.toggleDarkTheme(event);
+    expect(document.body.classList.contains("dark")).toBe(false);
   });
 });

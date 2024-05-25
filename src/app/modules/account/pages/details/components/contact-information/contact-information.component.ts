76dcc06060649a400d2598c3ac127f68be54d6a0
mufniarz <marcinmu330@gmail.com>
2024-05-25 20:37:22 +0800

    updated the contact information component

--- src/app/modules/account/pages/details/components/contact-information/contact-information.component.ts
+++ src/app/modules/account/pages/details/components/contact-information/contact-information.component.ts
@@ -18,7 +18,12 @@
 * along with Nonprofit Social Networking Platform.  If not, see <https://www.gnu.org/licenses/>.
 ***********************************************************************************************/
 import {Component, Input} from "@angular/core";
-import {Account} from "../../../../../../models/account.model";
+import {
+  Account,
+  Address,
+  Email,
+  PhoneNumber,
+} from "../../../../../../models/account.model";
 
 @Component({
   selector: "app-contact-information",
@@ -28,9 +33,16 @@ import {Account} from "../../../../../../models/account.model";
 })
 export class ContactInformationComponent {
   _account!: Partial<Account>;
+  showMore = {
+    phone: false,
+    email: false,
+    address: false,
+  };
+
   get account() {
     return this._account;
   }
+
   @Input() set account(account: Partial<Account> | undefined) {
     if (!account) {
       return;
@@ -38,11 +50,72 @@ export class ContactInformationComponent {
     this._account = account;
   }
 
-  getPhoneNumber(): string {
-    if (this.account?.contactInformation?.phoneNumbers.length === 0) {
+  get hasMoreThanOnePhoneNumber(): boolean {
+    return (
+      this.account?.contactInformation?.phoneNumbers?.length !== undefined &&
+      this.account.contactInformation.phoneNumbers.length > 1
+    );
+  }
+
+  get hasMoreThanOneEmail(): boolean {
+    return (
+      this.account?.contactInformation?.emails?.length !== undefined &&
+      this.account.contactInformation.emails.length > 1
+    );
+  }
+
+  get hasMoreThanOneAddress(): boolean {
+    return (
+      this.account?.contactInformation?.addresses?.length !== undefined &&
+      this.account.contactInformation.addresses.length > 1
+    );
+  }
+
+  getFirstPhoneNumber(): string {
+    const phoneNumbers = this.account?.contactInformation?.phoneNumbers;
+    if (!phoneNumbers || phoneNumbers.length === 0) {
       return "-";
     }
-    const firstPhoneInfos = this.account?.contactInformation?.phoneNumbers[0];
-    return `+${firstPhoneInfos?.countryCode}${firstPhoneInfos?.number}`;
+    return this.formatPhoneNumber(phoneNumbers[0]);
+  }
+
+  formatPhoneNumber(phone: PhoneNumber): string {
+    if (!phone.number) return "-";
+    const formattedNumber = phone.number.replace(
+      /(\d{3})(\d{3})(\d{4})/,
+      "$1 $2-$3",
+    );
+    const emergencyIndicator = phone.isEmergencyNumber ? "(E)" : "";
+    return `${phone.type || ""}${emergencyIndicator}: ${phone.countryCode || ""} ${formattedNumber}`;
+  }
+
+  getFirstEmail(): string {
+    const emails = this.account?.contactInformation?.emails;
+    if (!emails || emails.length === 0) {
+      return "-";
+    }
+    return this.formatEmail(emails[0]);
+  }
+
+  formatEmail(email: Email): string {
+    if (!email.email) return "-";
+    return `${email.name || "N/A"}: ${email.email}`;
+  }
+
+  getFirstAddress(): string {
+    const addresses = this.account?.contactInformation?.addresses;
+    if (!addresses || addresses.length === 0) {
+      return "-";
+    }
+    return addresses[0] ? this.formatAddress(addresses[0]) : "-";
+  }
+
+  formatAddress(address: Address): string {
+    if (!address.street) return "-";
+    return `${address.name || ""}\n${address.street}\n${address.city || ""}, ${address.state || ""}, ${address.country || ""} ${address.zipcode || ""}`;
+  }
+
+  toggleShow(type: "phone" | "email" | "address") {
+    this.showMore[type] = !this.showMore[type];
   }
 }

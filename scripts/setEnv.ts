094eafb0d9af25f5d229b23524205c3b1953d53f
mufniarz <marcinmu330@gmail.com>
2024-06-03 01:54:52 +0800

    updated setEnv to write the evn file

--- scripts/setEnv.ts
+++ scripts/setEnv.ts
@@ -21,26 +21,39 @@ import * as dotenv from "dotenv";
 import * as fs from "fs";
 import * as path from "path";
 
+// Load environment variables from .env file
 dotenv.config();
 
-const targetPath = path.resolve(
-  __dirname,
-  "../src/environments/environment.prod.ts",
-);
-const envConfigFile = `
+const envConfig = {
+  FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+// Create the environment.ts file content
+const envFileContent = `
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: '${process.env.FIREBASE_API_KEY}',
-    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
-    projectId: '${process.env.FIREBASE_PROJECT_ID}',
-    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
-    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
-    appId: '${process.env.FIREBASE_APP_ID}',
-    measurementId: '${process.env.FIREBASE_MEASUREMENT_ID}'
-  }
+    apiKey: '${envConfig["FIREBASE_API_KEY"]}',
+    authDomain: '${envConfig["FIREBASE_AUTH_DOMAIN"]}',
+    projectId: '${envConfig["FIREBASE_PROJECT_ID"]}',
+    storageBucket: '${envConfig["FIREBASE_STORAGE_BUCKET"]}',
+    messagingSenderId: '${envConfig["FIREBASE_MESSAGING_SENDER_ID"]}',
+    appId: '${envConfig["FIREBASE_APP_ID"]}',
+    measurementId: '${envConfig["FIREBASE_MEASUREMENT_ID"]}',
+  },
 };
 `;
 
-fs.writeFileSync(targetPath, envConfigFile);
-console.log(`Wrote variables to ${targetPath}`);
+// Write the environment.ts file
+const envFilePath = path.resolve(
+  __dirname,
+  "../src/environments/environment.prod.ts",
+);
+fs.writeFileSync(envFilePath, envFileContent);
+console.log(`Environment file created at ${envFilePath}`);

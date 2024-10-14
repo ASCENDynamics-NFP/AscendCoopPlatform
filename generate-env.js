bb3fdc327099735987a999662cfd495ae1f000f0
mufniarz <marcinmu330@gmail.com>
2024-10-14 10:54:18 +0800

    updated deployment scripts

---
+++ generate-env.js
@@ -0,0 +1,76 @@
+/***********************************************************************************************
+* Nonprofit Social Networking Platform: Allowing Users and Organizations to Collaborate.
+* Copyright (C) 2023  ASCENDynamics NFP
+*
+* This file is part of Nonprofit Social Networking Platform.
+*
+* Nonprofit Social Networking Platform is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* Nonprofit Social Networking Platform is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+
+* You should have received a copy of the GNU Affero General Public License
+* along with Nonprofit Social Networking Platform.  If not, see <https://www.gnu.org/licenses/>.
+***********************************************************************************************/
+// generate-env.js
+
+const dotenv = require("dotenv");
+const fs = require("fs");
+const path = require("path");
+
+// Determine the environment
+const env = process.env.NODE_ENV || 'development';
+
+// Load environment variables from the appropriate .env file (only in local development)
+if (process.env.CI !== 'true') {
+  const envFilePath = path.resolve(process.cwd(), `.env.${env}`);
+  dotenv.config({ path: envFilePath });
+}
+
+// Verify that required environment variables are set
+const requiredEnvVars = [
+  'FIREBASE_API_KEY',
+  'FIREBASE_AUTH_DOMAIN',
+  'FIREBASE_PROJECT_ID',
+  'FIREBASE_STORAGE_BUCKET',
+  'FIREBASE_MESSAGING_SENDER_ID',
+  'FIREBASE_APP_ID',
+  'FIREBASE_MEASUREMENT_ID'
+];
+
+requiredEnvVars.forEach((varName) => {
+  if (!process.env[varName]) {
+    console.warn(`Warning: ${varName} is not set.`);
+  }
+});
+
+// Set the target path based on the environment
+const targetPath = env === 'production'
+  ? './src/environments/environment.prod.ts'
+  : './src/environments/environment.ts';
+
+// Generate the environment configuration file content
+const envConfigFile = `
+export const environment = {
+  production: ${env === 'production'},
+  firebaseConfig: {
+    apiKey: '${process.env.FIREBASE_API_KEY}',
+    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
+    projectId: '${process.env.FIREBASE_PROJECT_ID}',
+    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
+    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
+    appId: '${process.env.FIREBASE_APP_ID}',
+    measurementId: '${process.env.FIREBASE_MEASUREMENT_ID}',
+  }
+};
+`;
+
+// Write the configuration to the appropriate environment file
+fs.writeFileSync(targetPath, envConfigFile);
+
+console.log(`Environment file generated at ${targetPath}`);

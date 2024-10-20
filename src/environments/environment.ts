2e41c27f8d7420bd009fdaa0066de9785262c3d2
mufniarz <marcinmu330@gmail.com>
2024-10-20 11:40:38 +0800

    added add domain script

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,47 +1,13 @@
-/***********************************************************************************************
-* Nonprofit Social Networking Platform: Allowing Users and Organizations to Collaborate.
-* Copyright (C) 2023  ASCENDynamics NFP
-*
-* This file is part of Nonprofit Social Networking Platform.
-*
-* Nonprofit Social Networking Platform is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as published
-* by the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
 
-* Nonprofit Social Networking Platform is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
-
-* You should have received a copy of the GNU Affero General Public License
-* along with Nonprofit Social Networking Platform.  If not, see <https://www.gnu.org/licenses/>.
-***********************************************************************************************/
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-// Not sure if I should be including this information in the project, but it is to the Dev environment.
-// If anyone has any suggestions on how to move these variables to somewhere more secure, please open an issue or discussion.
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyATq5A6OaHtXtDSRoC1DZ8T_xegCZiUocU",
-    authDomain: "ascendcoopplatform-dev.firebaseapp.com",
-    databaseURL: "https://ascendcoopplatform-dev-default-rtdb.firebaseio.com",
-    projectId: "ascendcoopplatform-dev",
-    storageBucket: "ascendcoopplatform-dev.appspot.com",
-    messagingSenderId: "360409127691",
-    appId: "1:360409127691:web:b1c488ce58f4e9330ca00e",
-    measurementId: "G-9EKFS37KV8",
-  },
+    apiKey: 'AIzaSyATq5A6OaHtXtDSRoC1DZ8T_xegCZiUocU',
+    authDomain: 'ascendcoopplatform-dev.firebaseapp.com',
+    projectId: 'ascendcoopplatform-dev',
+    storageBucket: 'ascendcoopplatform-dev.appspot.com',
+    messagingSenderId: '360409127691',
+    appId: '1:360409127691:web:b1c488ce58f4e9330ca00e',
+    measurementId: 'G-9EKFS37KV8',
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.

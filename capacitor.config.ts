35723d33a9992f7a8283f11a837a14879c3ed65d
mufniarz <marcinmu330@gmail.com>
2024-11-13 06:35:54 +0800

    reverting back to public from www, self inflicted wound

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import {CapacitorConfig} from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "io.ionic.starter",
   appName: "ascendcoopplatform",
-  webDir: "www",
+  webDir: "public",
   server: {
     androidScheme: "https",
   },

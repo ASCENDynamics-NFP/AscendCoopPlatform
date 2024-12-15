34dcdfcdf54fa0014b9388e0e40173c6cdc560e1
mufniarz <marcinmu330@gmail.com>
2024-12-15 23:26:56 +0400

    added landing page listing component

---
+++ src/app/core/services/listings.service.ts
@@ -0,0 +1,62 @@
+/***********************************************************************************************
+* Nonprofit Social Networking Platform: Allowing Users and Organizations to Collaborate.
+* Copyright (C) 2023  ASCENDynamics NFP
+*
+* This file is part of Nonprofit Social Networking Platform.
+*
+* Nonprofit Social Networking Platform is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* Nonprofit Social Networking Platform is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+
+* You should have received a copy of the GNU Affero General Public License
+* along with Nonprofit Social Networking Platform.  If not, see <https://www.gnu.org/licenses/>.
+***********************************************************************************************/
+// src/app/core/services/listings.service.ts
+
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {Listing} from "../../models/listing.model";
+import {environment} from "../../../environments/environment";
+
+/** Listings Service */
+@Injectable({
+  providedIn: "root",
+})
+export class ListingsService {
+  private endpoint = `${environment.firebaseConfig.apiUrl}/getHomepageListings`;
+
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Fetch homepage listings with user's location
+   * @param location User's browser location (latitude, longitude)
+   * @param limit Number of listings to fetch (default: 10)
+   * @param status Listing status filter (default: active)
+   * @param category Optional category filter
+   */
+  getHomepageListings(
+    location: {latitude: number; longitude: number},
+    limit: number = 4,
+    status: string = "active",
+    category?: string,
+  ): Observable<Listing[]> {
+    let params = new HttpParams()
+      .set("limit", limit)
+      .set("status", status)
+      .set("latitude", location.latitude)
+      .set("longitude", location.longitude);
+
+    if (category) {
+      params = params.set("category", category);
+    }
+
+    return this.http.get<Listing[]>(this.endpoint, {params});
+  }
+}
--- src/app/modules/auth/pages/landing/components/homepage-listings/homepage-listings.component.spec.ts
+++ src/app/modules/auth/pages/landing/components/homepage-listings/homepage-listings.component.spec.ts
@@ -17,17 +17,27 @@
 * You should have received a copy of the GNU Affero General Public License
 * along with Nonprofit Social Networking Platform.  If not, see <https://www.gnu.org/licenses/>.
 ***********************************************************************************************/
-export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-    apiUrl: process.env.FIREBASE_API_URL,
-  },
-};
+import {ComponentFixture, TestBed, waitForAsync} from "@angular/core/testing";
+import {IonicModule} from "@ionic/angular";
+
+import {HomepageListingsComponent} from "./homepage-listings.component";
+
+describe("HomepageListingsComponent", () => {
+  let component: HomepageListingsComponent;
+  let fixture: ComponentFixture<HomepageListingsComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [HomepageListingsComponent],
+      imports: [IonicModule.forRoot()],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HomepageListingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});

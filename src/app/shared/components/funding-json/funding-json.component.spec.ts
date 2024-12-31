63c8a3cd36c8198f5d4ca702d4e6bef7ed8af13b
mufniarz <marcinmu330@gmail.com>
2024-12-31 16:54:11 +0100

    fixed unit tests

--- src/app/shared/components/funding-json/funding-json.component.spec.ts
+++ src/app/shared/components/funding-json/funding-json.component.spec.ts
@@ -1,24 +1,69 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+/***********************************************************************************************
+* Nonprofit Social Networking Platform: Allowing Users and Organizations to Collaborate.
+* Copyright (C) 2023  ASCENDynamics NFP
+*
+* This file is part of Nonprofit Social Networking Platform.
+*
+* Nonprofit Social Networking Platform is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
 
-import { FundingJsonComponent } from './funding-json.component';
+* Nonprofit Social Networking Platform is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
 
-describe('FundingJsonComponent', () => {
+* You should have received a copy of the GNU Affero General Public License
+* along with Nonprofit Social Networking Platform.  If not, see <https://www.gnu.org/licenses/>.
+***********************************************************************************************/
+import {TestBed} from "@angular/core/testing";
+import {FundingJsonComponent} from "./funding-json.component";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import {HttpClient} from "@angular/common/http";
+
+describe("FundingJsonComponent", () => {
   let component: FundingJsonComponent;
-  let fixture: ComponentFixture<FundingJsonComponent>;
+  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ FundingJsonComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+      imports: [HttpClientTestingModule],
+      declarations: [FundingJsonComponent],
+    });
 
-    fixture = TestBed.createComponent(FundingJsonComponent);
+    const fixture = TestBed.createComponent(FundingJsonComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+    httpClient = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
 
-  it('should create', () => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it("should create the component", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should log an error if funding.json fails to load", () => {
+    const consoleErrorSpy = spyOn(console, "error");
+
+    component.ngOnInit();
+
+    const req = httpMock.expectOne("/funding.json");
+    expect(req.request.method).toBe("GET");
+
+    // Simulate error response
+    req.error(new ErrorEvent("Network error"));
+
+    expect(consoleErrorSpy).toHaveBeenCalledWith(
+      "Error fetching funding.json:",
+      jasmine.any(Object),
+    );
   });
 });

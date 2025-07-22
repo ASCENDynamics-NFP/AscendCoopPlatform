8b15559fce692831054a0625e8a601fa70514381
Marcin Ufniarz <marcinmu330@gmail.com>
2025-07-21 22:08:07 -0500

    Display accounts in role hierarchy

--- src/app/modules/account/pages/role-hierarchy/role-hierarchy.page.ts
+++ src/app/modules/account/pages/role-hierarchy/role-hierarchy.page.ts
@@ -1,20 +1,29 @@
 import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Store} from "@ngrx/store";
-import {Observable, combineLatest, of} from "rxjs";
+import {Observable, combineLatest} from "rxjs";
 import {map} from "rxjs/operators";
-import {RelatedAccount} from "@shared/models/account.model";
+import {Account, RelatedAccount} from "@shared/models/account.model";
 import {GroupRole} from "@shared/models/group-role.model";
 import {
   selectGroupRolesByGroupId,
   selectRelatedAccountsByAccountId,
+  selectAccountEntities,
 } from "../../../../state/selectors/account.selectors";
 import * as AccountActions from "../../../../state/actions/account.actions";
+
+interface DisplayAccount {
+  id: string | undefined;
+  name?: string;
+  tagline?: string;
+  iconImage?: string;
+  roleName?: string;
+}
 
 interface TreeNode {
   role?: GroupRole;
   children: TreeNode[];
-  accounts: Partial<RelatedAccount>[];
+  accounts: DisplayAccount[];
 }
 
 @Component({
@@ -40,18 +49,26 @@ export class RoleHierarchyPage implements OnInit {
     const relatedAccounts$ = this.store.select(
       selectRelatedAccountsByAccountId(this.groupId),
     );
+    const accountEntities$ = this.store.select(selectAccountEntities);
 
+    this.store.dispatch(AccountActions.loadAccounts());
     this.store.dispatch(AccountActions.loadGroupRoles({groupId: this.groupId}));
     this.store.dispatch(
       AccountActions.loadRelatedAccounts({accountId: this.groupId}),
     );
 
-    this.tree$ = combineLatest([roles$, relatedAccounts$]).pipe(
-      map(([roles, related]) => this.buildTree(roles, related)),
+    this.tree$ = combineLatest([roles$, relatedAccounts$, accountEntities$]).pipe(
+      map(([roles, related, entities]) =>
+        this.buildTree(roles, related, entities),
+      ),
     );
   }
 
-  buildTree(roles: GroupRole[], related: Partial<RelatedAccount>[]): TreeNode {
+  buildTree(
+    roles: GroupRole[],
+    related: Partial<RelatedAccount>[],
+    entities: {[id: string]: Account},
+  ): TreeNode {
     const roleMap = new Map<string, TreeNode>();
     roles.forEach((role) => {
       roleMap.set(role.id, {role, children: [], accounts: []});
@@ -78,7 +95,16 @@ export class RoleHierarchyPage implements OnInit {
       .filter((ra) => ra.status === "accepted" && ra.roleId)
       .forEach((ra) => {
         const node = roleMap.get(ra.roleId!);
-        if (node) node.accounts.push(ra);
+        if (!node) return;
+        const account = entities[ra.id];
+        const display: DisplayAccount = {
+          id: ra.id,
+          name: account?.name || ra.name,
+          tagline: account?.tagline || ra.tagline,
+          iconImage: account?.iconImage || ra.iconImage,
+          roleName: ra.role || node.role?.name,
+        };
+        node.accounts.push(display);
       });
 
     return root;

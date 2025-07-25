d01a50700c7478eb33652f95416c028ea95577aa
Marcin Ufniarz <marcinmu330@gmail.com>
2025-07-25 02:45:48 -0500

    refactor(projects): use store actions and selectors

--- src/app/modules/account/pages/projects/projects.page.spec.ts
+++ src/app/modules/account/pages/projects/projects.page.spec.ts
@@ -3,34 +3,29 @@ import {IonicModule} from "@ionic/angular";
 import {ProjectsPage} from "./projects.page";
 import {provideMockStore, MockStore} from "@ngrx/store/testing";
 import {ActivatedRoute} from "@angular/router";
-import {ProjectService} from "../../../../core/services/project.service";
 import {SuccessHandlerService} from "../../../../core/services/success-handler.service";
 import {ErrorHandlerService} from "../../../../core/services/error-handler.service";
 import {selectAuthUser} from "../../../../state/selectors/auth.selectors";
 import {selectRelatedAccountsByAccountId} from "../../../../state/selectors/account.selectors";
+import {
+  selectProjectsByAccount,
+  selectActiveProjectsByAccount,
+} from "../../../../state/selectors/projects.selectors";
+import * as ProjectsActions from "../../../../state/actions/projects.actions";
 import {of} from "rxjs";
 
 describe("ProjectsPage", () => {
   let component: ProjectsPage;
   let fixture: ComponentFixture<ProjectsPage>;
   let store: MockStore;
-  let serviceSpy: jasmine.SpyObj<ProjectService>;
   let errorHandler: {handleFirebaseAuthError: jasmine.Spy};
 
   beforeEach(async () => {
-    serviceSpy = jasmine.createSpyObj("ProjectService", [
-      "getProjects",
-      "createProject",
-      "updateProject",
-      "setArchived",
-    ]);
-
     await TestBed.configureTestingModule({
       declarations: [ProjectsPage],
       imports: [IonicModule.forRoot()],
       providers: [
         provideMockStore(),
-        {provide: ProjectService, useValue: serviceSpy},
         {
           provide: SuccessHandlerService,
           useValue: {handleSuccess: jasmine.createSpy("handleSuccess")},
@@ -49,7 +44,11 @@ describe("ProjectsPage", () => {
     store = TestBed.inject(MockStore);
     store.overrideSelector(selectAuthUser, {uid: "u1"} as any);
     store.overrideSelector(selectRelatedAccountsByAccountId("acc1"), []);
-    serviceSpy.getProjects.and.returnValue(of([]));
+    store.overrideSelector(selectProjectsByAccount("acc1"), []);
+    store.overrideSelector(selectActiveProjectsByAccount("acc1"), []);
+
+    spyOn(store, "dispatch").and.callThrough();
+
     errorHandler = TestBed.inject(ErrorHandlerService) as any;
 
     fixture = TestBed.createComponent(ProjectsPage);
@@ -62,14 +61,15 @@ describe("ProjectsPage", () => {
   });
 
   it("should load projects for account", () => {
-    expect(serviceSpy.getProjects).toHaveBeenCalledWith("acc1");
+    expect(store.dispatch).toHaveBeenCalledWith(
+      ProjectsActions.loadProjects({accountId: "acc1"}),
+    );
   });
 
   it("should show error when adding a project with duplicate name", () => {
-    // simulate existing active project
-    serviceSpy.getProjects.and.returnValue(
-      of([{id: "p1", name: "Alpha"} as any]),
-    );
+    store.overrideSelector(selectActiveProjectsByAccount("acc1"), [
+      {id: "p1", name: "Alpha"} as any,
+    ]);
     fixture = TestBed.createComponent(ProjectsPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -78,13 +78,16 @@ describe("ProjectsPage", () => {
     component.addProject();
 
     expect(errorHandler.handleFirebaseAuthError).toHaveBeenCalled();
-    expect(serviceSpy.createProject).not.toHaveBeenCalled();
+    expect(store.dispatch).not.toHaveBeenCalledWith(
+      jasmine.objectContaining({type: ProjectsActions.createProject.type}),
+    );
   });
 
   it("should show error when updating to a duplicate name", () => {
-    serviceSpy.getProjects.and.returnValue(
-      of([{id: "p1", name: "Alpha"} as any, {id: "p2", name: "Beta"} as any]),
-    );
+    store.overrideSelector(selectActiveProjectsByAccount("acc1"), [
+      {id: "p1", name: "Alpha"} as any,
+      {id: "p2", name: "Beta"} as any,
+    ]);
     fixture = TestBed.createComponent(ProjectsPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -93,6 +96,8 @@ describe("ProjectsPage", () => {
     component.updateProject(project, " alpha ");
 
     expect(errorHandler.handleFirebaseAuthError).toHaveBeenCalled();
-    expect(serviceSpy.updateProject).not.toHaveBeenCalled();
+    expect(store.dispatch).not.toHaveBeenCalledWith(
+      jasmine.objectContaining({type: ProjectsActions.updateProject.type}),
+    );
   });
 });

1a55cdd3c2ae7eeec00d2bcc6a5d0a3c0a4f8b10
Marcin Ufniarz <marcinmu330@gmail.com>
2025-07-25 11:38:42 -0500

    test: mock store for info page specs

--- src/app/modules/info/pages/event-calendar/event-calendar.page.spec.ts
+++ src/app/modules/info/pages/event-calendar/event-calendar.page.spec.ts
@@ -2,6 +2,7 @@ import {ComponentFixture, TestBed, waitForAsync} from "@angular/core/testing";
 import {IonicModule} from "@ionic/angular";
 import {RouterTestingModule} from "@angular/router/testing";
 import {SharedModule} from "../../../../shared/shared.module";
+import {provideMockStore} from "@ngrx/store/testing";
 import {EventCalendarPage} from "./event-calendar.page";
 
 describe("EventCalendarPage", () => {
@@ -12,6 +13,14 @@ describe("EventCalendarPage", () => {
     TestBed.configureTestingModule({
       declarations: [EventCalendarPage],
       imports: [IonicModule.forRoot(), RouterTestingModule, SharedModule],
+      providers: [
+        provideMockStore({
+          initialState: {
+            auth: {user: null, loading: false, error: null},
+            projects: {entities: {}, loading: false, error: null},
+          },
+        }),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EventCalendarPage);

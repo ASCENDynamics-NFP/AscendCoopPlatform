09af2f14eff948954100cdb16184f498d860a2ef
Marcin Ufniarz <marcinmu330@gmail.com>
2025-07-25 00:27:47 -0500

    chore: add license headers for onCreate tests and trigger

--- functions/src/database/accounts/triggers/onCreate/index.ts
+++ functions/src/database/accounts/triggers/onCreate/index.ts
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with Nonprofit Social Networking Platform.  If not, see <https://www.gnu.org/licenses/>.
  ********************************************************************************/
+// functions/src/database/accounts/triggers/onCreate/index.ts
 
 import {
   onDocumentCreated,

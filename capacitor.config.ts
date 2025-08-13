851cf166b0caef21ce866d5e27d838dbe3971c1d
mufniarz <marcinmu330@gmail.com>
2025-08-13 00:16:46 -0500

    initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -21,7 +21,7 @@ import {CapacitorConfig} from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "org.ascendcoopplatform.app",
-  appName: "ASCENDynamics NFP Cooperative Platform",
+  appName: "ASCENDynamics NFP Platform",
   webDir: "public",
   server: {
     androidScheme: "https",
